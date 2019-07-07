06023587000cf055d1e6fbc93973bfd72b9de83d
harshabhadra <harshasharkey@gmail.com>
2019-07-07 21:20:23 +0530

    api adjustment

--- app/src/main/java/com/example/android/foodie/Config.java
+++ app/src/main/java/com/example/android/foodie/Config.java
@@ -2,5 +2,5 @@ package com.example.android.foodie;
 
 public class Config {
 
-    public static final String DEVELOPER_KEY = "AIzaSyBkGwcOfhwbejl0UXQmtj3QaJdRCl3KLt0";
+    public static final String DEVELOPER_KEY = "";
 }
